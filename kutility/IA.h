cda719c2419d7a0d33ad6bb2356fde40e6def7e0
spyfox <spyfox@malfeu.fr>
2015-08-03 23:53:29 +0200

    add kutility

---
+++ kutility/IA.h
@@ -0,0 +1,11 @@
+namespace kutility
+{
+    class IA
+    {
+    public:
+        virtual int getValue() const ;
+        virtual void setValue(int value);
+        virtual void print() const;
+    };
+} // kutility
+
